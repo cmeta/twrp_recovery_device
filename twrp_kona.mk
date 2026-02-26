fdc278dee5c993e92675511ae5717f439154b998
cmeta <36048601+cmeta@users.noreply.github.com>
2026-02-26 19:35:11 +0800

    Update twrp_kona.mk

--- twrp_kona.mk
+++ twrp_kona.mk
@@ -16,7 +16,7 @@ $(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, device/qualcomm/kona/device.mk)
 
 PRODUCT_DEVICE := kona
-PRODUCT_NAME := omni_kona
+PRODUCT_NAME := twrp_kona
 PRODUCT_BRAND := qti
 PRODUCT_MODEL := Kona for arm64
 PRODUCT_MANUFACTURER := qualcomm
